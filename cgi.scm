7eed73049e9aa50bdb66614771ed832ba7502521
Thien-Thi Nguyen <ttn@gnuvola.org>
2003-02-12 13:57:50 +0000

    Update copyright; nfc.

--- cgi.scm
+++ cgi.scm
@@ -1,6 +1,6 @@
 ;;; www/cgi.scm --- Common Gateway Interface support for WWW scripts
 
-;; 	Copyright (C) 1997,2001,2002 Free Software Foundation, Inc.
+;; 	Copyright (C) 1997,2001,2002,2003 Free Software Foundation, Inc.
 ;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
