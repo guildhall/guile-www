2078be2480c1d4ead944b1ca8191c6512a85c312
Thien-Thi Nguyen <ttn@gnuvola.org>
2004-01-12 00:22:59 +0000

    Add usage comment, w/ info on auto* tools versions. Before calling automake, set up dummy doc/guile-www.texi file. No longer pass "--verbose" to automake.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+#
+# usage: sh -x autogen.sh
+#
+# tested with:
+# - automake 1.7.6
+# - autoconf 2.58
 
 [ -f configure.in ] || {
   echo "autogen.sh: run this command only at the top of a source tree."
@@ -7,4 +13,15 @@
 
 aclocal
 autoconf
-automake --add-missing --verbose
+
+# automake is not so smooth handling generated .texi
+texi='doc/guile-www.texi'
+if [ ! -f $texi ] ; then
+    echo '@setfilename guile-www.info' > $texi
+    echo '@include version.texi' >> $texi
+    touch -m 01010000 $texi
+fi
+
+automake --add-missing
+
+# autogen.sh ends here
