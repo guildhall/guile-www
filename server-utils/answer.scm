280b59307f57edd9c8abfa2bf1c34db684008100
Thien-Thi Nguyen <ttn@gnuvola.org>
2007-01-28 11:29:41 +0000

    Update years in copyright notice; nfc.

--- server-utils/answer.scm
+++ server-utils/answer.scm
@@ -1,6 +1,6 @@
 ;; www/server-utils/answer.scm --- HTTP connection handling and responses
 ;;
-;; 	Copyright (C) 2004,2006 Free Software Foundation, Inc.
+;; 	Copyright (C) 2004,2006,2007 Free Software Foundation, Inc.
 ;;
 ;; This is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
