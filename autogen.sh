e3dde9f3811a9010c95de6bc1a71aa35b32b907a
Thien-Thi Nguyen <ttn@gnuvola.org>
2010-06-27 15:40:03 +0200

    [maint] Use autoreconf(1); make bootstrap script non-executable.
    * autogen.sh: Remove executable perm bits (chmod -x);
    don't symlink guile.m4 and don't run aclocal(1),
    autoconf(1), automake(1); instead, run autoreconf(1).

--- autogen.sh
+++ autogen.sh
@@ -5,16 +5,12 @@
 # tested with:
 # - autoconf (GNU Autoconf) 2.65
 # - automake (GNU automake) 1.11.1
-# - Guile 1.4.1.118
+# - Guile 1.4.1.119
 
 [ -f configure.ac ] || {
   echo "autogen.sh: run this command only at the top of a source tree."
   exit 1
 }
-
-ln -sf $(guile-config info datadir)/aclocal/guile.m4 build-aux/guile.m4
-aclocal -I build-aux
-autoconf
 
 # automake is not so smooth handling generated .texi
 texi='doc/guile-www.texi'
@@ -24,6 +20,6 @@ if [ ! -f $texi ] ; then
     touch -r configure.ac $texi
 fi
 
-automake --add-missing
+autoreconf --verbose --install --symlink -Wall
 
 # autogen.sh ends here
