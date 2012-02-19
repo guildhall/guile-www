3fd8b69d3ccfef3b02d785f0ca671ea8cc1310cb
Thien-Thi Nguyen <ttn@gnuvola.org>
2012-02-19 15:10:31 +0100

    [int] Import ‘(ice-9 rdelim) read-line’ explicitly.
    * source/http.scm: Import ‘(ice-9 rdelim) read-line’;
    remove conditional ‘use-modules’ form.

--- source/http.scm
+++ source/http.scm
@@ -44,14 +44,12 @@
             http:request)
   #:use-module ((srfi srfi-13) #:select ((substring/shared . subs)))
   #:use-module (www url)
+  #:use-module ((ice-9 rdelim) #:select (read-line))
   #:use-module (ice-9 regex)
   #:use-module (ice-9 rw))
 
 
 ;;; Compatibility
-
-(or (defined? 'read-line)
-    (use-modules (ice-9 rdelim)))
 
 (define (fs s . args)
   (apply simple-format #f s args))
