2ec702f08fdb7f7ff2077ddb7161ff4771ea63c4
Thien-Thi Nguyen <ttn@gnuvola.org>
2014-10-08 09:07:04 +0200

    [maint] Use URL in license notice last paragraph; nfc.

--- source/server-utils/cgi-prep.scm
+++ source/server-utils/cgi-prep.scm
@@ -15,10 +15,8 @@
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 ;;
-;; You should have received a copy of the GNU General Public
-;; License along with Guile-WWW; see the file COPYING.  If not,
-;; write to the Free Software Foundation, Inc., 51 Franklin Street,
-;; Fifth Floor, Boston, MA  02110-1301  USA
+;; You should have received a copy of the GNU General Public License
+;; along with Guile-WWW.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (www server-utils cgi-prep)
   #:export (cgi-environment-manager))
